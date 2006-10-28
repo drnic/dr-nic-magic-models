1f336c227c44cf98815a7866be3d0224ce13f8d2
nicwilliams <nicwilliams@de3bcc45-6e19-0410-94f5-955a5e68fca2>
2006-10-28 21:00:56 +0000

    validates_numericality_of uses column.null instead of hardcoded true
    git-svn-id: svn+ssh://rubyforge.org/var/svn/magicmodels/dr_nic_magic_models/trunk@37 de3bcc45-6e19-0410-94f5-955a5e68fca2

--- lib/dr_nic_magic_models/validations.rb
+++ lib/dr_nic_magic_models/validations.rb
@@ -12,14 +12,14 @@ module DrNicMagicModels
       self.columns.reject { |column| column.name =~ /(?i)^(((created|updated)_(at|on))|position|type|id)$/ }.each do |column|
         
         if column.type == :integer   
-          logger.debug "validates_numericality_of #{column.name}, :allow_nil => true, :only_integer => true"       
-          self.validates_numericality_of column.name, :allow_nil => true, :only_integer => true
+          logger.debug "validates_numericality_of #{column.name}, :allow_nil => #{column.null.inspect}, :only_integer => true"       
+          self.validates_numericality_of column.name, :allow_nil => column.null, :only_integer => true
         elsif column.number?
-          logger.debug "validates_numericality_of #{column.name}, :allow_nil => true"
-          self.validates_numericality_of column.name, :allow_nil => true
+          logger.debug "validates_numericality_of #{column.name}, :allow_nil => #{column.null.inspect}"
+          self.validates_numericality_of column.name, :allow_nil => column.null
         elsif column.text? && column.limit
-          logger.debug "validates_length_of #{column.name}, :allow_nil => true, :maximum => #{column.limit}"
-          self.validates_length_of column.name, :allow_nil => true, :maximum => column.limit
+          logger.debug "validates_length_of #{column.name}, :allow_nil => #{column.null.inspect}, :maximum => #{column.limit}"
+          self.validates_length_of column.name, :allow_nil => column.null, :maximum => column.limit
         end
         
         # Active record seems to interpolate booleans anyway to either true, false or nil...
